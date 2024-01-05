9ce2a814ab2e0b35501f060a5ac5f87fa6a13330
Andrew Haarsager <athaarsager@gmail.com>
2024-01-05 16:35:25 -0600

    Removed old comments, added new ones to script

---
+++ script.js
@@ -0,0 +1,13 @@
+// Collect input for:
+// employee firstName
+// employee lastName
+// employee ID number
+// employee job title
+// employee annual salary
+
+// Store the above information in an array of employee objects, then:
+// Append each employee's information in the table, add delete button, and clear input fields onSubmit
+// Create function that updates the monthly costs (total cost variable) with each employee added (remember, MONTHLY cost)
+// --IF total monthly costs > 20k, add red background to total monthly cost
+// --(Stretch mode: adjust total monthly cost on delete)
+// update README.md!!!
